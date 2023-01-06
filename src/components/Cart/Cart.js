eb3452037a5f1e801bcbc481c67f3479006fc3b6
RyanWatts1 <devshop202@yahoo.com>
2023-01-06 14:57:52 -0500

    removing hard coded data and using context with totalAmount

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,17 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const CartCtx = useContext(CartContext);
+
+  const totalAmount = `$${CartCtx.totalAmount.toFixed(2)}`;
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {CartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -15,7 +22,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
